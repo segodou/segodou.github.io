0b484dfcd1ee29252372b9263a5c39ad4df3e291
Ulrich Segodo <segodoulrich21@gmail.com>
2024-04-07 04:27:09 +0200

    add new css et js files

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,13 @@
-/*!
-* Start Bootstrap - Personal v1.0.1 (https://startbootstrap.com/template-overviews/personal)
-* Copyright 2013-2023 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-personal/blob/master/LICENSE)
-*/
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll(".nav-link");
+
+    navLinks.forEach(function(navLink) {
+        navLink.addEventListener("mouseover", function() {
+            this.classList.add("hovered"); // Ajoute la classe "hovered" au survol
+        });
+
+        navLink.addEventListener("mouseout", function() {
+            this.classList.remove("hovered"); // Supprime la classe "hovered" lorsque la souris quitte la zone
+        });
+    });
+});
